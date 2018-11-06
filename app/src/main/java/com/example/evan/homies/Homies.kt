e3abc5dd053d4ddca6a7e9c71af1b79d24c9aabe
exf6514 <exf6514@cs.rit.edu>
2018-11-06 15:01:47 -0500

    added layout fragment and dummy log in functionality

--- app/src/main/java/com/example/evan/homies/Homies.kt
+++ app/src/main/java/com/example/evan/homies/Homies.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_homies.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class Homies : AppCompatActivity() {
+
+    private var userId: Int? = null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         var selectedFragment: Fragment? = null
@@ -29,7 +36,29 @@ class Homies : AppCompatActivity() {
 
         //TODO: get global user id? If not logged in, send to login fragment
 
-        //by default, set the fragment to chore list
+        if(userId == null) {
+            // send to login
+            setContentView(R.layout.fragment_login)
+        } else {
+            //by default, set the fragment to chore list
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ChoresFragment()).commit()
+        }
+
+
+    }
+
+    fun logIn(v: View){
+
+        val inflator: LayoutInflater = layoutInflater
+
+        val email:String = emailEditText.text.toString()
+        val password:String = passwordEditText.text.toString()
+
+        Toast.makeText(this, "Hello $email", Toast.LENGTH_LONG).show()
+
+        //TODO: change this is real userId after fetch
+        userId = 1
+        setContentView(R.layout.activity_homies)
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ChoresFragment()).commit()
     }
 }
---
+++ app/src/main/java/com/example/evan/homies/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.example.evan.homies
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_login.*
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
